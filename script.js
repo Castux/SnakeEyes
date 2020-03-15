72c362da7099c84e50bfcd50bff09defa670acc6
Noé Falzon <falzon.noe@gmail.com>
2020-03-15 23:55:51 +0200

    Started Fengari integration: can run scripts!

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const luaconf  = fengari.luaconf;
+const lua      = fengari.lua;
+const lauxlib  = fengari.lauxlib;
+const lualib   = fengari.lualib;
+
+
 var myTextarea = document.getElementById("code");
 var editor = CodeMirror.fromTextArea(myTextarea, {
     lineNumbers: true,
@@ -8,7 +14,25 @@ var output = document.getElementById("output");
 
 function run()
 {
-    var text = document.createTextNode(editor.doc.getValue());
+    var script = editor.doc.getValue();
+
+    const L = lauxlib.luaL_newstate();
+    lualib.luaL_openlibs(L);
+    var status = lauxlib.luaL_loadstring(L, fengari.to_luastring(script));
+
+    var result = "";
+
+    if(status == lua.LUA_OK)
+    {
+        lua.lua_call(L, 0, 0);
+        result = "Did it!";
+    }
+    else
+    {
+        result = "Error occured";
+    }
+
+    var text = document.createTextNode(result);
     output.innerHTML = "";
     output.appendChild(text);
 }
